d4c58bb46b7aa3029a5c2a68340b451194f62402
lorenzejay <lorenzejaytech@gmail.com>
2025-04-22 09:59:39 -0700

    using routers

--- flow_use_case/src/flow_use_case/main.py
+++ flow_use_case/src/flow_use_case/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from pydantic import BaseModel
-from crewai.flow.flow import Flow, listen, start
-
+from crewai.flow.flow import Flow, listen, start, router
+from crewai.llm import LLM
 from .crews.lead_qualification_crew.lead_qualification_crew import (
     LeadQualificationCrew,
     LeadPersonalInfo,
@@ -53,25 +53,55 @@ class LeadQualificationFlow(Flow[LeadQualificationState]):
         self.state.personal_info = result.pydantic.personal_info
         self.state.company_info = result.pydantic.company_info
 
-    @listen(generate_lead_score)
-    def generate_lead_email(self):
+    @router(generate_lead_score)
+    def check_approval(self, previous_result):
         if self.state.lead_score.score > 70:
-            print("state", self.state)
-            result = (
-                EmailEngagementCrew()
-                .crew()
-                .kickoff(
-                    inputs={
-                        "personal_info": str(self.state.personal_info),
-                        "company_info": str(self.state.company_info),
-                        "lead_score": str(self.state.lead_score.score),
-                    }
-                )
+            return "approved"
+        else:
+            return "rejected"
+
+    @listen("approved")
+    def generate_lead_email(self):
+        print("state", self.state)
+        result = (
+            EmailEngagementCrew()
+            .crew()
+            .kickoff(
+                inputs={
+                    "personal_info": str(self.state.personal_info),
+                    "company_info": str(self.state.company_info),
+                    "lead_score": str(self.state.lead_score.score),
+                }
             )
+        )
 
         print("Email generated", result.raw)
         self.state.lead_score = result.raw
         self.state.personal_info = result.raw
+
+    @listen("rejected")
+    def generate_rejection_email(self):
+        result = LLM(model="gpt-4o-mini").call(
+            messages=[
+                {
+                    "role": "system",
+                    "content": f"""
+            You are a sales agent.
+            You are given a lead that was rejected.
+            You need to generate a rejection email to the lead.
+            The lead is:
+            {self.state.personal_info}
+            {self.state.company_info}
+            {self.state.lead_score}
+            """,
+                },
+                {
+                    "role": "user",
+                    "content": "Generate a rejection email to the lead.",
+                },
+            ]
+        )
+        print("Rejection email generated", result)
 
 
 def kickoff():
